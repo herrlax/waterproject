f4596a1da728f2bc1b506feaf919bf4627277bde
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2017-02-13 15:18:44 +0100

    initial commit

---
+++ waterproject.ino
@@ -0,0 +1,22 @@
+// outputs
+const int pump      = 6; 
+const int motor     = 7;
+const int led_water = 1;
+const int led_map   = 2;
+
+unsigned long currentMillis;
+unsigned long previousMillis;
+unsigned long duration;
+unsigned long hour = 3600000UL; // one hour
+unsigned long duration2 ;
+unsigned long pumpstart ;
+
+void setup() {
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
