0b159bdd58120c12d9d4ffb04897b6d64443d590
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2017-02-13 16:23:59 +0100

    add basic structure for pump

--- waterproject.ino
+++ waterproject.ino
@@ -1,25 +1,46 @@
-// outputs
-const int pump      = 6; 
-const int motor     = 7;
-const int led_water = 1;
-const int led_map   = 2;
+// led outputs
+const int led_water1  = 1;
+const int led_water2  = 2;
+const int led_map1    = 3;
+const int led_map2    = 4;
+const int led_map3    = 5;
+
+// motor pump outputs
+const int pumpOutput  = 6; 
+const int motor       = 7;
 
 unsigned long currentMillis;
 unsigned long previousMillis;
 unsigned long duration;
-unsigned long hour = 3600000UL; // one hour
-unsigned long duration2 ;
+unsigned long second = 1000; // one second in millis
 unsigned long pumpstart ;
 
 int counter = 0;
 
 void setup() {
-  pinMode (pump, OUTPUT) ;
-  pinMode (motor, OUTPUT) ;
-  duration = millis();
+  pinMode(pumpOutput, OUTPUT);
+  pinMode(motor, OUTPUT);
+  pinMode(led_water1, OUTPUT);
+  pinMode(led_water2, OUTPUT);
+  pinMode(led_map1, OUTPUT);
+  pinMode(led_map2, OUTPUT);
+  pinMode(led_map3, OUTPUT);
+  duration  = millis();
+  pumpstart = millis() ;
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  // read rfid
+  // pump(value from tag);
 }
+
+// run if an rfid has been read 
+void pump(int duration) {
+  
+  digitalWrite(pumpOutput, HIGH);  
+  delay(duration);                
+  digitalWrite(pumpOutput, LOW);   
+}
+
