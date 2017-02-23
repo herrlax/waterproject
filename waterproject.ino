d27ee8bbf40610447d61b91226b6d9815482453a
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2017-02-23 10:02:13 +0100

    add logic for motor handing

--- waterproject.ino
+++ waterproject.ino
@@ -10,7 +10,8 @@ String tagString;
 char tagNumber[14];
 boolean receivedTag;
 
-int currentDelay = 0;
+int currentDelay_w = 0;
+int currentDelay_m = 0;
 
 
 // led outputs
@@ -20,9 +21,9 @@ const int led_map1    = 3;
 const int led_map2    = 4;
 const int led_map3    = 5;
 
-// motor pump outputs
+// motorOutput pump outputs
 const int pumpOutput  = 6; 
-const int motor       = 7;
+const int motorOutput = 7;
 
 unsigned long currentMillis;
 unsigned long previousMillis;
@@ -34,7 +35,7 @@ int counter = 0;
 
 void setup() {
   pinMode(pumpOutput, OUTPUT);
-  pinMode(motor, OUTPUT);
+  pinMode(motorOutput, OUTPUT);
   pinMode(led_water1, OUTPUT);
   pinMode(led_water2, OUTPUT);
   pinMode(led_map1, OUTPUT);
@@ -63,56 +64,109 @@ void loop() {
     tagString = tagNumber;
     Serial.println();
     
-    int delayTime = 0;
-    delayTime = determineDelay(tagNumber);
-    Serial.println("delayTime: ");
-    Serial.println(delayTime);
+    int delayTime_w = 0; // delay time for filling the water
+    int delayTime_m = 0; // delay time for the motor to control the tactile indicator
     
-    Serial.println("currentDelay: ");
-    Serial.println(currentDelay);
+    delayTime_w = determineDelayW(tagNumber);
+    delayTime_m = determineDelayM(tagNumber);
     
-    if(delayTime == currentDelay) {
+    Serial.println("delayTime_w: ");
+    Serial.println(delayTime_w);
+    Serial.println("delayTime_m: ");
+    Serial.println(delayTime_m);
+    
+    Serial.println("currentDelay_w: ");
+    Serial.println(currentDelay_w);
+    Serial.println("currentDelay_m: ");
+    Serial.println(currentDelay_m);
+
+    // water level unchanged..
+    if(delayTime_w == currentDelay_w) {
       return;
     }
     
     
-    pump(delayTime - currentDelay);
-
-    currentDelay = abs(delayTime);
+    pump((delayTime_w - currentDelay_w), (delayTime_m - currentDelay_m));
+    currentDelay_w = abs(delayTime_w);
+    currentDelay_m = abs(delayTime_m);
   }
 }
 
-int determineDelay(const char *tagNum) {
-      
-    if(strcmp(tagNum+1, tag_0) == 0) {
+// returns water delay time depending on tagnumber
+int determineDelayW(const char *tagNum) {
+
+    if(strcmp(tagNum+1, tag_0) == 0) { // tagNum+1, since it starts with a blank
+      return 3000;
+      Serial.println("FOUND");
+    }
+    
+    if(strcmp(tagNum+1, tag_1) == 0) { // tagNum+1, since it starts with a blank
+      return 4000;
+      Serial.println("FOUND");
+    }
+
+    // tag not recognized
+    return 0;
+}
+
+
+// returns motor delay time depending on tagnumber
+int determineDelayM(const char *tagNum) {
+
+    if(strcmp(tagNum+1, tag_0) == 0) { // tagNum+1, since it starts with a blank
       return 1000;
       Serial.println("FOUND");
     }
     
-    if(strcmp(tagNum+1, tag_1) == 0) {
+    if(strcmp(tagNum+1, tag_1) == 0) { // tagNum+1, since it starts with a blank
       return 2000;
       Serial.println("FOUND");
     }
       
-    return 5000;
+    // tag not recognized
+    return 0;
 }
 
-// run if an rfid has been read 
-void pump(int duration) {
+// pumps water changes the water indicator ..
+void pump(int duration_w, int duration_m) {
   
   Serial.println();
-  Serial.print("Received in pump:");
-
-  // remove water..
-  if(duration < 0) {
-    Serial.println();
+  
+  if(duration_w < 0) {
     Serial.print("remove water: ");
-  } else if (duration > 0) {
-    Serial.println();
+    Serial.println(abs(duration_w));
+    Serial.print("lower the indicator: ");
+    Serial.println(abs(duration_m));
+  } else if (duration_w > 0) {
     Serial.print("add water: ");
+    Serial.println(abs(duration_w));
+    Serial.print("raise the indicator: ");
+    Serial.println(abs(duration_m));
   }
   
-  Serial.println(abs(duration));
+  Serial.println();
+
+  // keep both water pump and motor on for their shared time,
+  // asuming the motor always should be on for a shorter amount of
+  // time than the waterpump
+  //digitalWrite(pumpOutput, HIGH);
+  //digitalWrite(motorOutput, HIGH);
+  Serial.println("POWER PUMP..");
+  Serial.println("POWER MOTOR..");
+  delay(duration_m);
+
+  // turn the motor for the indicator off, and keep the pump on..
+  //digitalWrite(motorOutput, LOW);
+  Serial.println("POWER PUMP..");
+  delay(duration_w - duration_m);
+
+  // turn off the waterpump too..
+  //digitalWrite(pumpOutput, LOW); 
+  
+  Serial.println("BOTH OFF PUMP AND MOTOR OFF..");
+  
+  Serial.println();
+
   rfidReader.flush();
 }
 
